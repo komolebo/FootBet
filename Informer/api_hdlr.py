3d6fbafe28264574424fc5fb37f87394a4569b0b
Oleh Komarnytskyi <komolebo@gmail.com>
2018-07-07 03:20:13 +0300

    Add connection via session id, add match analyze

--- Informer/api_hdlr.py
+++ Informer/api_hdlr.py
@@ -1,146 +1,30 @@
-import re
-import urllib2
-from time import sleep
-
-from nvm import Match, Club, Player, NVM
 from BeautifulSoup import BeautifulSoup
 
-
-# .....
-class CommonAPI:
-    url = 'https://www.statbunker.com/'
-    ip = '84.47.179.79'
-
-    def __init__(self):
-        pass
-
-
-class PlayerAPI(Player, CommonAPI):
-    def __init__(self, ID):
-        Player.__init__(self, ID)
-
-
-class MatchAPI(Match, CommonAPI):
-    # def __init__(self, ID, name, league_name, comp_id, date):
-    def __init__(self, ID, match_url):
-        Match.__init__(self, ID)
-        self.match_url = match_url
-        # self.league_name = league_name
-        # self.comp_id = comp_id
-        # self.date = date
-        # self.name = name
-
-    # def get_url(self):
-    #     return self.url + ("competitions/MatchDetails/%s" % self.league_name)
-
-
-class ClubAPI(Club, CommonAPI):
-    def __init__(self, ID):
-        Club.__init__(self, ID)
-
-
-class HTML_Season:
-    def __init__(self, comp_id, name):
-        self.name = name
-        self.comp_id = comp_id
-
-
-class HTML_League:
-    def __init__(self, url, name):
-        self.url = url
-        self.name = name
-        self.seasons = []
-
-
-api_leagues = []
-
-
-def req_url(url):
-    print 'requesting', url
-    headers = {}
-    # headers = {'User-Agent': 'Mozilla/5.0'}
-    req = urllib2.Request(url, None, headers)
-    resp = urllib2.urlopen(req)
-    sleep(2)
-    return resp.read()
-
-
-def analyze_match_page(url):
-    print 'analyzing %s' % url
-
-    page = req_url(url)
-    parsed_page = BeautifulSoup(page)
-
-    # score
-    score_tag = parsed_page.findAll('div', {'class': 'matchReportTitle'})
-    # for i in score_tag:
-    #     print i #score_tag
-    print score_tag
-    home_team = None
-    away_team = None
-    score = None
+from Informer.api_data import req_url, CommonAPI
+from Informer.api_parse import get_leagues, get_seasons_for_leagues, get_matches_from_season
 
 
 # ********* algorithms to update local data ********* #
 def deep_check():  # walk through main leagues found on main url
     # Search all leagues
-    page = req_url(CommonAPI.url)
-    parsed_page = BeautifulSoup(page)
+    main_page = req_url(CommonAPI.url)
+    parsed_main_page = BeautifulSoup(main_page)
 
-    html_leagues = parsed_page.findAll('div', {'class': 'intRight'})
-    for div in html_leagues:
-        tags = div.findAll('a')
-        for tag in tags:
-            league_href = tag['href']
-            league_name = tag.find('span').text
-            api_leagues.append(HTML_League(league_href, league_name))
+    api_leagues = get_leagues(parsed_main_page)
 
     # search all seasons in leagues
-    for league_obj in api_leagues:
-        page = req_url(league_obj.url)
-        parsed_page = BeautifulSoup(page)
-        # print league_obj.name
-
-        # search all seasons for league
-        seasons_url = parsed_page.findAll('select', {'name': 'comp_id'})
-        for season in seasons_url:
-            for option in season.findAll('option'):
-                comp_id = option['value']
-                # if not 'Select' in option.text:
-                if int(comp_id) >= 0:
-                    league_obj.seasons.append(HTML_Season(comp_id, option.text))
-            # print season.findAll('option')
-        # TODO
-        break
+    get_seasons_for_leagues(api_leagues)
 
     # search all matches in league season
-    tmp = 0
     for league_obj in api_leagues:
         for season_obj in league_obj.seasons:
-            if not tmp:
-                tmp = 1
-                continue
-            # TODO
-            url = CommonAPI.url + ('competitions/LastMatches?comp_id=%s&limit=10' % season_obj.comp_id)
-
-            # Here we should grab all matches from season
-            page = req_url(url)
-            parsed_page = BeautifulSoup(page)
-
-            html_matches = parsed_page.findAll('span', {'class': 'matchVs'})
-            for html_match in html_matches:
-                match_url = html_match.find('a')['href']
-                analyze_match_page(match_url)
-
-            # for season in league_obj.seasons:
-            # matches_url = parsed_page.findAll('a', {'title': season + ' Latest results'})
-            # print matches_url, season
-
-            break
+            get_matches_from_season(league_obj, season_obj)
         # print matches_url
 
 
 if __name__ == '__main__':
+    # analyze_match_page(
+    #     'https://www.statbunker.com/competitions/MatchDetails/Premier-League-17/18/West-Ham-United-VS-Everton?comp_id=586&match_id=95485&date=13-May-2018')
     deep_check()
     # for league in api_leagues:
     #     print league
---
+++ Informer/api_parse.py
@@ -0,0 +1,126 @@
+from BeautifulSoup import BeautifulSoup
+
+from Informer.api_data import req_url, HTML_League, HTML_Season, CommonAPI
+
+
+def analyze_match_page(url):
+    print 'analyzing %s' % url
+
+    page = req_url(url)
+    match_page = BeautifulSoup(page)
+
+    away_team, home_team, score = analyze_match_team_score(match_page)
+    print "%s vs %s [%s]" % (home_team, away_team, score)
+
+    date, referee, stadium, time, visitee = analyze_match_general_info(match_page)
+    print date, time, stadium, referee, visitee
+
+    team_home, team_away, sub_home, sub_away = analyze_match_squads(match_page)
+    # print team_home, team_away, sub_home, sub_away
+    for player1, player2 in zip(team_home, team_away):
+        print "!%30s %30s!" % (player1[0], player2[0])
+
+
+def analyze_match_general_info(match_page):
+    general_tags = match_page.findAll('div', {'class': 'matchStatsInt'})
+    date = general_tags[0].text
+    time = general_tags[1].text
+    stadium = general_tags[2].text
+    referee = general_tags[3].text
+    visitee = general_tags[4].text
+    return date, referee, stadium, time, visitee
+
+
+def analyze_match_team_score(parsed_page):
+    # score
+    score_tags = parsed_page.findAll('div', {'class': 'matchReportTitle'})
+    # for i in score_tag:
+    #     print i #score_tag
+    score_tag = score_tags[0]
+    score_tags = score_tag.findAll('h2')
+    home_team = score_tags[0].text
+    away_team = score_tags[2].text
+    score = score_tags[1].text
+    return [away_team, home_team, score]
+
+
+def analyze_tag_player(player_tag):
+    sub_ref = player_tag.find('a')['href']
+    id = sub_ref.split('player_id=', 1)[1]
+    player_data = player_tag.find('div', {'class': 'playerName'}).text
+    name, position = player_data.split(',')
+    return name, position, id
+
+
+def analyze_squad(squad):
+    squad_players = []
+    for player_tag in squad:
+        player = analyze_tag_player(player_tag)
+        squad_players.append(player)
+    return squad_players
+
+
+def analyze_match_squads(match_page):
+    match_squads_tags = match_page.findAll('ul', {'class': 'matchSquads'})
+    # 0 home, 1 away, 2 home sub, 3 away sub
+    home_squad = analyze_squad(match_squads_tags[0].findAll('li'))
+    away_squad = analyze_squad(match_squads_tags[1].findAll('li'))
+    sub_home_squad = analyze_squad(match_squads_tags[2].findAll('li'))
+    sub_away_squad = analyze_squad(match_squads_tags[3].findAll('li'))
+    return home_squad, away_squad, sub_home_squad, sub_away_squad
+
+
+def get_leagues(parsed_page):
+    html_leagues = parsed_page.findAll('div', {'class': 'intRight'})
+    leagues = []
+    for div in html_leagues:
+        tags = div.findAll('a')
+        for tag in tags:
+            league_href = tag['href']
+            league_name = tag.find('span').text
+            leagues.append(HTML_League(league_href, league_name))
+    return leagues
+
+
+def get_seasons_for_league(league_obj, parsed_league_page):
+    # search all seasons for league
+    seasons_url = parsed_league_page.findAll('select', {'name': 'comp_id'})
+    for season in seasons_url:
+        for option in season.findAll('option'):
+            comp_id = option['value']
+            # if not 'Select' in option.text:
+            if int(comp_id) >= 0:
+                league_obj.seasons.append(HTML_Season(comp_id, option.text))
+        # print season.findAll('option')
+    # TODO
+
+
+def get_seasons_for_leagues(api_leagues):
+    for league_obj in api_leagues:
+        league_page = req_url(league_obj.url)
+        parsed_league_page = BeautifulSoup(league_page)
+        get_seasons_for_league(league_obj, parsed_league_page)
+
+
+def get_matches_from_season(league_obj, season_obj):
+    for offset in range(0, 1000, 10):
+        try:
+            url = CommonAPI.url + (
+                    'competitions/LastMatches?comp_id=%s&offset=%s' % (season_obj.comp_id, str(offset)))
+            # Here we should grab all matches from season
+            page = req_url(url)
+            parsed_page = BeautifulSoup(page)
+
+            html_matches = parsed_page.findAll('span', {'class': 'matchVs'})
+            if not len(html_matches):
+                break
+            for html_match in html_matches:
+                match_url = html_match.find('a')['href']
+                analyze_match_page(match_url)
+        except Exception:
+            print 'Pages end for league=%u season=%u' % (league_obj.name, season_obj.name)
+            break
+
+        # for season in league_obj.seasons:
+        # matches_url = parsed_page.findAll('a', {'title': season + ' Latest results'})
+        # print matches_url, season
